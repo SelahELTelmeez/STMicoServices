a9d4a51e707101178957b8ef875fa26f9c626c10
Ahmed Abuelnour <Ahmed.Abuelnoor@outlook.com>
2022-03-11 23:24:14 +0200

    Edit Resemd Mobile Verification

--- IdentityInfrastructure/Features/ResendMobileVerification/CQRS/Command/ResendMobileVerificationCommandHandler.cs
+++ IdentityInfrastructure/Features/ResendMobileVerification/CQRS/Command/ResendMobileVerificationCommandHandler.cs
@@ -14,15 +14,15 @@ public class ResendMobileVerificationCommandHandler : IRequestHandler<ResendMobi
 {
     private readonly STIdentityDbContext _dbContext;
     private readonly JsonLocalizerManager _resourceJsonManager;
-    private readonly INotificationService _notificationEmailService;
+    private readonly INotificationService _notificationService;
     private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public ResendMobileVerificationCommandHandler(STIdentityDbContext dbContext, JsonLocalizerManager resourceJsonManager, 
-                                                  INotificationService notificationEmailService, IHttpContextAccessor httpContextAccessor)
+    public ResendMobileVerificationCommandHandler(STIdentityDbContext dbContext, JsonLocalizerManager resourceJsonManager,
+                                                  INotificationService notificationService, IHttpContextAccessor httpContextAccessor)
     {
         _dbContext = dbContext;
         _resourceJsonManager = resourceJsonManager;
-        _notificationEmailService = notificationEmailService;
+        _notificationService = notificationService;
         _httpContextAccessor = httpContextAccessor;
     }
 
@@ -43,21 +43,62 @@ public class ResendMobileVerificationCommandHandler : IRequestHandler<ResendMobi
         else
         {
             //2.0 ReSend Email Verification Code.
-            _ = _notificationEmailService.SendEmailAsync(new EmailNotificationModel
+            if (string.IsNullOrEmpty(identityUser.MobileNumber))
             {
-                MailFrom = "noreply@selaheltelmeez.com",
-                MailTo = identityUser.Email,
-                MailSubject = "سلاح التلميذ - رمز التفعيل",
-                IsBodyHtml = true,
-                DisplayName = "سلاح التلميذ",
-                MailToName = identityUser.FullName,
-                MailBody = UtilityGenerator.GetOTP(4).ToString()
+                return new CommitResult
+                {
+                    ErrorCode = "X0000",
+                    ErrorMessage = _resourceJsonManager["X0000"]
+                };
+            }
+            // Check SMS Limit per day.
+            await _dbContext.Entry(identityUser).Collection(a => a.Activations).LoadAsync(cancellationToken);
+
+            if (identityUser.Activations.Where(a => (DateTime.UtcNow > a.CreatedOn) && (DateTime.UtcNow < DateTime.UtcNow.EndOfDay())).Count() >= 3)
+            {
+                return new CommitResult
+                {
+                    ErrorCode = "X0000", // Exceed the limit of SMS for today.
+                    ErrorMessage = _resourceJsonManager["X0000"],
+                    ResultType = ResultType.Unauthorized
+                };
+            }
+
+            // Generate OTP
+            IdentityActivation identityActivation = new IdentityActivation
+            {
+                ActivationType = ActivationType.Mobile,
+                Code = UtilityGenerator.GetOTP(4).ToString(),
+                IdentityUserId = identityUser.Id
+            };
+            _dbContext.Set<IdentityActivation>().Add(identityActivation);
+
+            await _dbContext.SaveChangesAsync();
+
+            bool result = await _notificationService.SendSMSAsync(new SMSNotificationModel
+            {
+                MobileNumber = identityUser.MobileNumber,
+                OTPCode = identityActivation.Code
             }, cancellationToken);
 
-            return new CommitResult
+            if (result)
             {
-                ResultType = ResultType.Ok
-            };
+                return new CommitResult
+                {
+                    ResultType = ResultType.Ok
+                };
+            }
+            else
+            {
+                return new CommitResult
+                {
+                    ErrorCode = "X0000", // Couldn't send a SMS Message
+                    ErrorMessage = _resourceJsonManager["X0000"],
+                    ResultType = ResultType.Invalid
+                };
+            }
         }
+
     }
+
 }
---
+++ IdentityInfrastructure/Utilities/DateExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityInfrastructure.Utilities
+{
+    public static class DateExtensions
+    {
+        public static DateTime EndOfDay(this DateTime theDate)
+             => theDate.Date.AddDays(1).AddTicks(-1);
+    }
+}
