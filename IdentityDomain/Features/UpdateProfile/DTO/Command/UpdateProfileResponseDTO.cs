789730ee588abbd28e65a85f8dd2bbaf54b25ef7
Ahmed <AhmedAbuelnoor@outlook.com>
2022-03-20 20:54:04 +0200

    0

--- IdentityDomain/Features/UpdateProfile/DTO/Command/UpdateProfileResponseDTO.cs
+++ IdentityDomain/Features/UpdateProfile/DTO/Command/UpdateProfileResponseDTO.cs
@@ -5,16 +5,14 @@ public class UpdateProfileResponseDTO
     public string FullName { get; set; }
     public string? Email { get; set; }
     public string? MobileNumber { get; set; }
-    public string Grade { get; set; }
-    public string Gender { get; set; }
-    public string Country { get; set; }
-    public DateTime DateOfBirth { get; set; }
+    public string? Grade { get; set; }
+    public string? Gender { get; set; }
+    public string? Country { get; set; }
+    public DateTime? DateOfBirth { get; set; }
     public string ReferralCode { get; set; }
     public bool IsPremium { get; set; }
     public string Role { get; set; }
-    public string Governorate { get; set; }
+    public string? Governorate { get; set; }
     public string AvatarUrl { get; set; }
-    public string AccessToken { get; set; }
-    public string RefreshToken { get; set; }
     public bool IsVerified { get; set; }
 }
