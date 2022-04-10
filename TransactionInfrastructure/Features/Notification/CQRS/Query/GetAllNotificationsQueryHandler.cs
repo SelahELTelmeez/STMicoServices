eba748e3a47ba9f8567b40abb92f8f4a54156510
Ahmed <AhmedAbuelnoor@outlook.com>
2022-04-10 09:51:39 +0200

    .GetIdentityUserId()

--- TransactionInfrastructure/Features/Notification/CQRS/Query/GetAllNotificationsQueryHandler.cs
+++ TransactionInfrastructure/Features/Notification/CQRS/Query/GetAllNotificationsQueryHandler.cs
@@ -11,19 +11,19 @@ namespace TransactionInfrastructure.Features.Notification.CQRS.Query;
 public class GetAllNotificationsQueryHandler : IRequestHandler<GetAllNotificationsQuery, CommitResults<NotificationResponse>>
 {
     private readonly TrackerDbContext _dbContext;
-    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly Guid? _userId;
 
     public GetAllNotificationsQueryHandler(TrackerDbContext dbContext, IHttpContextAccessor httpContextAccessor)
     {
         _dbContext = dbContext;
-        _httpContextAccessor = httpContextAccessor;
+        _userId = httpContextAccessor.GetIdentityUserId();
     }
     public async Task<CommitResults<NotificationResponse>> Handle(GetAllNotificationsQuery request, CancellationToken cancellationToken)
         => new CommitResults<NotificationResponse>
         {
             ResultType = ResultType.Ok,
             Value = await _dbContext.Set<DomainEntities.Notification>()
-                                   .Where(a => a.NotifierId.Equals(_httpContextAccessor.GetIdentityUserId()))
+                                   .Where(a => a.NotifierId.Equals(_userId))
                                    .ProjectToType<NotificationResponse>()
                                    .ToListAsync(cancellationToken)
         };
