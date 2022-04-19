d860f8ee98742ad6fc752913c38cc8909d65dfe2
Ahmed <AhmedAbuelnoor@outlook.com>
2022-04-19 15:58:11 +0200

    0

--- TransactionInfrastructure/HttpClients/IdentityClient.cs
+++ TransactionInfrastructure/HttpClients/IdentityClient.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.Configuration;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using TransactionDomain.Features.Parent.DTO;
-using TransactionDomain.Features.Shared.DTO;
 using TransactionInfrastructure.Utilities;
 
 namespace TransactionInfrastructure.HttpClients
@@ -17,24 +16,6 @@ namespace TransactionInfrastructure.HttpClients
             _httpClient.BaseAddress = new Uri(configuration["IdentityClient:baseUrl"]);
             _httpClient.DefaultRequestHeaders.Add("Accept-Language", httpContextAccessor.GetAcceptLanguage());
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", httpContextAccessor.GetJWTToken());
-        }
-
-        public async Task<CommitResult<LimitedProfileResponse>?> GetIdentityLimitedProfileAsync(Guid IdentityId, CancellationToken cancellationToken)
-        {
-            return await _httpClient.GetFromJsonAsync<CommitResult<LimitedProfileResponse>>($"/Identity/GetIdentityLimitedProfile?IdentityId={IdentityId}", cancellationToken);
-        }
-
-        public async Task<CommitResults<LimitedProfileResponse>?> GetIdentityLimitedProfilesAsync(IEnumerable<Guid> IdentityIds, CancellationToken cancellationToken)
-        {
-            HttpResponseMessage responseMessage = await _httpClient.PostAsJsonAsync("/Identity/GetIdentityLimitedProfiles", IdentityIds, cancellationToken);
-
-            return await responseMessage.Content.ReadFromJsonAsync<CommitResults<LimitedProfileResponse>>(cancellationToken: cancellationToken);
-        }
-
-        public async Task<CommitResults<LimitedProfileResponse>?> GetTeacherLimitedProfilesByNameOrMobileNumberAsync(string NameOrMobile, CancellationToken cancellationToken)
-        {
-            return await _httpClient.GetFromJsonAsync<CommitResults<LimitedProfileResponse>>($"/Identity/GetTeacherLimitedProfilesByNameOrMobile?NameOrMobile={NameOrMobile}", cancellationToken);
-
         }
 
         public async Task<CommitResult<AddParentChildResponse>?> AddParentChildAsync(AddParentChildRequest request, CancellationToken cancellationToken)
