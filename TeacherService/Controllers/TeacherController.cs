013db262d0b4ce22982f644a146eaafe7633976a
Ahmed <AhmedAbuelnoor@outlook.com>
2022-04-19 16:16:06 +0200

    Adding multiple actions in teacher controller

--- TeacherService/Controllers/TeacherController.cs
+++ TeacherService/Controllers/TeacherController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TeacherDomain.Features.Classes.CQRS.Command;
+using TeacherDomain.Features.Classes.DTO.Command;
 using TeacherDomain.Features.TeacherClass.DTO.Command;
 
 namespace TeacherService.Controllers;
@@ -20,5 +21,28 @@ public class TeacherController : ControllerBase
     [HttpPost("[action]")]
     public async Task<IActionResult> CreateClass([FromBody] CreateClassRequest createClassRequest, CancellationToken token)
         => Ok(await _mediator.Send(new CreateClassCommand(createClassRequest), token));
+
+
+    [HttpPut("[action]")]
+    public async Task<IActionResult> UpdateClass([FromBody] UpdateClassRequest updateClassRequest, CancellationToken token)
+        => Ok(await _mediator.Send(new UpdateClassCommand(updateClassRequest), token));
+
+    [HttpPut("[action]")]
+    public async Task<IActionResult> ToggleActivateClass([FromBody] int ClassId, CancellationToken token)
+         => Ok(await _mediator.Send(new ToggleActivateClassCommand(ClassId), token));
+
+
+    [HttpPost("[action]")]
+    public async Task<IActionResult> RequestToEnrollClass([FromBody] int ClassId, CancellationToken token)
+         => Ok(await _mediator.Send(new RequestEnrollToClassCommand(ClassId), token));
+
+    [HttpDelete("[action]")]
+    public async Task<IActionResult> UnrollFromClass([FromBody] int ClassId, CancellationToken token)
+         => Ok(await _mediator.Send(new UnrollFromClassCommand(ClassId), token));
+
+    [HttpDelete("[action]")]
+    public async Task<IActionResult> UnrollFromClassByTeacher([FromBody] UnrollStudentFromClassByTeacherRequest request, CancellationToken token)
+         => Ok(await _mediator.Send(new UnrollStudentFromClassByTeacherCommand(request), token));
+
 
 }
